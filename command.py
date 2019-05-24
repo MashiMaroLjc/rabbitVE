68f82424bab127ef6a2d4390b2756ab1c4e7fbb7
531041610@qq.com <531041610@qq.com>
2019-05-24 12:43:36 +0800

    update the command which plugin need

--- command.py
+++ command.py
@@ -3,3 +3,12 @@ video_cut = "ffmpeg -ss {} -to {} -i {} -codec copy -avoid_negative_ts 1 -y {}"
 
 video_merge="ffmpeg -f concat -safe 0 -i {} -c copy -y {}"
 # 参数依次为:要拼接的文件的txt,输出视频文件名字
+
+video_audio_combine = "ffmpeg -i {} -i {} -vcodec copy -acodec copy  -y {}"
+# 参数依次为:视频,音频，输出
+
+extract_audio = "ffmpeg  -i {}  -vn -y {}"
+# 参数依次为:视频,音频音频
+
+imgs2video = "ffmpeg -i {} -c:v libx264 -vf \"fps={},format=yuv420p\" -y {}"
+# 参数依次为:图片匹配字符 fps 输出
