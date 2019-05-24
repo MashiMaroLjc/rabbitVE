054214ad8e72590710e6bf14197b189927d1e11e
531041610@qq.com <531041610@qq.com>
2019-05-24 13:03:21 +0800

    fix the struct of code

--- lib/processor_plugin/__init__.py
+++
@@ -1,5 +0,0 @@
-from .target_face_detect import TargetFaceDetect
-
-processor_dict = {
-    "target_face_detect": TargetFaceDetect
-}
--- lib/processor_plugin/base_processor.py
+++
@@ -1,9 +0,0 @@
-class BaseProcessor:
-    def __init__(self):
-        pass
-
-    def initialize(self, params):
-        self.params = params
-
-    def transform(self, frame):
-        raise NotImplemented
